a426eb869934545a07360697cdc9cd4e2947c598
Felix-Yvonnet <felix.yvonnet@hotmail.fr>
2023-02-03 18:31:55 +0100

    Started case study

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,24 @@
 import sfml.graphics.*
 import sfml.window.*
-import sfml.system.Vector2f
+import sfml.system.Vector2
+
+
+/*
+val map = scala.collection.mutable.HashMap.empty[Int,String]
+
+val m2 = Map[String, Int]()
+val () = println(m2("1"))
+val () = println(m2 contains "1")
+*/
+
+class Input (val keyboard : Map[Keyboard.Key, Int],
+              var x : Int, var y : Int, var xrel : Int, var yrel : Int, 
+              var xwheel : Int, var ywheel : Int, 
+              var mouse : Array[Int]) 
+
+
+
+
 
 @main def main =
   val window = RenderWindow(VideoMode(1024, 768), "Hello world")
@@ -12,17 +30,24 @@ import sfml.system.Vector2f
 
   val sprite = Sprite(texture)
   sprite.move(500, 400)
-  // sprite.position = Vector2f(10, 320)
+  sprite.position = Vector2(10, 320)
   // sprite.rotation = 90.0
   // sprite.scale(0.3,0.3)
 
   val sprite2 = Sprite(texture)
   sprite2.move(300, 400)
-
+  
+  val status = Input(Map[Keyboard.Key,Int]().empty, 0,0,0,0,0,0,Array[Int](0,0,0,0,0,0))
   while window.isOpen() do
     for event <- window.pollEvent() do
       event match {
         case _: Event.Closed => window.closeWindow()
+        case Event.KeyPressed(c, _, _, _, _): Event.KeyPressed => status.keyboard.updated(c, 1)
+        case Event.KeyReleased(c, _, _, _, _): Event.KeyReleased => status.keyboard.updated(c, 0)
+        case Event.MouseMoved(x, y): Event.MouseMoved => status.x = x; status.y = y
+        case Event.MouseWheelScrolled(wheel, delta, x, y) => status.xwheel = x; status.ywheel = y
+        case Event.MouseButtonPressed(Mouse.Button.Left, x, y) => status.mouse(0) = 1
+        case Event.MouseButtonPressed(Mouse.Button.Right, x, y) => status.mouse(0) = 1
         case _ => ()
       }
 

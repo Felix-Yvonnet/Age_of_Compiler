a15777ceffcebce678ef34391877003ef3ab1760
bloubloub <joseph.lenormand@ens-paris-saclay.fr>
2023-02-23 19:58:08 +0100

    not trying to do anything else

--- src/main/scala/machine/object/movable/characters/characters.scala
+++ src/main/scala/machine/object/movable/characters/characters.scala
@@ -4,10 +4,10 @@ import sfml.system.Vector2
 import machine.`object`.movable.Movable
 
 class Player(sprite_path : String) extends Movable :
-    var waitTime = 0
-    private var next_pos : List[Vector2[Float]] = List()
+    private val waitPerMove = 0
+    private var stateMove = 0
+    private var nextPos : List[Vector2[Float]] = List()
     private var indice = 100
-
     def move(): Unit =
         next_pos match
             case List() => ()

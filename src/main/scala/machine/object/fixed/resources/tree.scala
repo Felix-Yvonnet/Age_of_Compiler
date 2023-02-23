b163c7c5a88d9b2cbf55156ad14571439d1c6fbb
Felix-Yvonnet <felix.yvonnet@hotmail.fr>
2023-02-24 00:23:14 +0100

    maybe sthg has changed idk

--- src/main/scala/machine/object/fixed/resources/tree.scala
+++ src/main/scala/machine/object/fixed/resources/tree.scala
@@ -2,7 +2,19 @@ package machine.`object`.fixed.resources
 
 import machine.`object`.GameObject
 import sfml.system.Vector2
+import machine.`object`.movable.characters.Player
 
 class tree(sprite_path : String = "src/resources/fixed_objects/tree_hand_made.png") extends GameObject(sprite_path) :
 
     pos = Vector2[Int](10,5)
+    val apportBéton = 10
+    val apportMoula = 0
+    waitTimeResources = 10
+    agility  = 10
+
+    def giveResources(player : Player) =
+        if waitTimeResources == 0 then
+            player.béton+=apportBéton
+            player.argent+=apportMoula
+            waitTimeResources = agility
+        else waitTimeResources -= 1
